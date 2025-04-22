f9a1e49d600bffe091ac18c133d5789a7b73fbbe
busaiv <busaiv@yahoo.com>
2025-04-22 20:30:59 +0300

    temp: switched to raw packet output for debugging

--- draft.py
+++ draft.py
@@ -1,19 +1,21 @@
 from scapy.all import get_if_list, get_if_addr, sniff
 import psutil
+#
+# if_list = get_if_list()
+# print(if_list)
+# for i in if_list:
+#     print(get_if_addr(i))
+#
+#
+# def choice_interface():
+#     interfaces = psutil.net_if_addrs()
+#     for i in interfaces:
+#         print(i)
+#     interface = input('Enter interface: ')
+#     return interface
+#
+# print(choice_interface())
+#
+# print(sniff(iface=choice_interface(), store=False))
 
-if_list = get_if_list()
-print(if_list)
-for i in if_list:
-    print(get_if_addr(i))
-
-
-def choice_interface():
-    interfaces = psutil.net_if_addrs()
-    for i in interfaces:
-        print(i)
-    interface = input('Enter interface: ')
-    return interface
-
-print(choice_interface())
-
-print(sniff(iface=choice_interface(), store=False))
+print(psutil.net_if_addrs())
--- main_logic/sniffer.py
+++ main_logic/sniffer.py
@@ -1,18 +1,21 @@
 import scapy.all as scapy
+import os
 import psutil
+from scapy.interfaces import ifaces
 from scapy.layers.inet import IP, TCP, UDP
 from scapy.layers.dns import DNS, DNSQR
 from tqdm import tqdm
 from colorama import Fore, Style
 
 requests = []
+requests.clear()
 
-def choice_interface():
-    interfaces = psutil.net_if_addrs()
-    for i in interfaces:
-        tqdm.write(f'{i}: {interfaces[i]}')
-    interface = input('Enter interface: ')
-    return interface
+# def choice_interface():
+#     interfaces = psutil.net_if_addrs()
+#     for i in interfaces:
+#         tqdm.write(f'{i}: {interfaces[i]}')
+#     interface = input('Enter interface: ')
+#     return interface
 
 def packets_saver(packet):
     request_id = len(requests) + 1
@@ -21,34 +24,16 @@ def packets_saver(packet):
 
 def packet_process(packet):
     tqdm.write(f'{Fore.LIGHTWHITE_EX}------New Packet------')
-    if packet.haslayer(IP):
-        source_ip = packet[IP].src
-        destination_ip = packet[IP].dst
-        tqdm.write(f'{Fore.LIGHTRED_EX}Source IP: {source_ip} -> Destination IP: {destination_ip}')
-
-    if packet.haslayer(TCP):
-        source_tcp = packet[TCP].sport
-        destination_tcp = packet[TCP].dport
-        tqdm.write(f'{Fore.LIGHTYELLOW_EX}Source TCP: {source_tcp} -> Destination TCP: {destination_tcp}')
-
-    if packet.haslayer(UDP):
-        source_udp = packet[UDP].sport
-        destination_udp = packet[UDP].dport
-        tqdm.write(f'{Fore.LIGHTGREEN_EX}Source UDP: {source_udp} -> Destination UDP: {destination_udp}')
-
-    if packet.haslayer(DNS) and packet.getlayer(DNS).qr == 0:
-        queried_host = packet[DNSQR].qname.decode()
-        tqdm.write(f"{Fore.CYAN}DNS Query: {queried_host}")
-
+    tqdm.write(f'{Fore.LIGHTCYAN_EX}Raw Packet Data: {packet.show(dump=True)}')
     tqdm.write(f'{Style.RESET_ALL}')
     tqdm.write(f'{Fore.LIGHTWHITE_EX}------Packet End------\n')
 
 def params(packet):
     packet_process(packet)
-    packets_saver(packet)
+    #packets_saver(packet)
 
 def sniffer():
-    scapy.sniff(store=False, prn=params, filter='udp')
+    scapy.sniff(iface=os.getenv('INTERFACE'), store=False, promisc=True, prn=params)
 
 if __name__ == '__main__':
     sniffer()
